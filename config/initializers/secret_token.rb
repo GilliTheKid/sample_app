6dd551f719490791e87d54d74f89c9d5f333b7b8
GtK <GilliTheKid@GtK-MA.local>
2011-05-31 13:31:59 +0200

    Fixed /config/initializers/secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,8 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-<<<<<<< HEAD
+
 SampleApp::Application.config.secret_token = '32cac9da722f6793397b844725dc2039989a68b3271f0952ff0d5bca853e8e15ed3c7f36f5dcd403b0e4c822c2f26e3a60a2fbbcfbed4a3063ed90a3373b90f5'
-=======
+
 SampleApp::Application.config.secret_token = 'c98206962665b1ced6f97028508a61948d7939843f285bd06e0b1179cc1709a43db30e146ff6bfd6ed623d1e663ccdd61770ca03f9e21ea468c6ac736b945c3a'
->>>>>>> 6e061d09748450e8382588fb48576c8c0b996e51
+
