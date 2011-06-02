122c2af466129584443b1fccad7ac3493605de0e
GtK <GilliTheKid@GtK-MA.local>
2011-06-02 16:15:00 +0200

    Added Robin Text

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+  end
+  
+  it  "should have the right title" do
+    get 'home'
+    response.should have_selector("title",
+                                :content => " | Home")
   end
 
   describe "GET 'contact'" do
@@ -15,5 +22,23 @@ describe PagesController do
       response.should be_success
     end
   end
-
+  
+  it "should have the right title" do
+    get 'contact'
+    response.should have_selector("title",
+                                :content => " | Contact")
+  end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success    
+    end
+  end    
+  
+  it "should have the right title" do
+    get 'about'
+    response.should have_selector("title",
+                                :content => " | About")
+  end                    
 end
